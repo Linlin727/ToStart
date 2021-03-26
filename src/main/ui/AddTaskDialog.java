3a0b83cf7023db553cbc3606d3c577577da79248
Jingxuan Ma <majingxuan511@163.com>
2021-03-26 09:04:31 +0800

    Finished P3 without audio

--- src/main/ui/AddTaskDialog.java
+++ src/main/ui/AddTaskDialog.java
@@ -1,76 +1,76 @@
-package ui;
-
-import model.Task;
-import model.ToDoList;
-
-import javax.swing.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class AddTaskDialog extends JDialog {
-    final int width = 700;
-    final int height = 700;
-    private ToDoList toDoList;
-
-    public AddTaskDialog(JFrame jf, String title, boolean isModel) {
-        super(jf, title, isModel);
-        toDoList = new ToDoList();
-
-        //组装视图
-        this.setLocation(200, 200);
-
-        Box box = Box.createVerticalBox();
-        //组装task
-        Box taskBox = Box.createHorizontalBox();
-        JLabel taskLabel = new JLabel("Task");
-        JTextField taskField = new JTextField(7);
-        taskBox.add(taskLabel);
-        taskBox.add(Box.createHorizontalStrut(20));
-        taskBox.add(taskField);
-
-        //组装State
-
-        Box stateBox = Box.createHorizontalBox();
-        JLabel stateLabel = new JLabel("State");
-        JTextField stateField = new JTextField(7);
-        stateBox.add(stateLabel);
-        stateBox.add(Box.createHorizontalStrut(20));
-        stateBox.add(stateField);
-
-        //组装按钮
-
-        Box buttonBox = Box.createHorizontalBox();
-        JButton addButton = new JButton("Add Task");
-        addButton.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                //get input
-                String task = taskField.getText().trim();
-                String state = stateField.getText().trim();
-                Task t = new Task(task, state);
-                toDoList.addTask(t);
-                dispose();
-
-            }
-        });
-        buttonBox.add(addButton);
-
-        //add together
-        box.add(Box.createVerticalStrut(20));
-        box.add(taskBox);
-        box.add(Box.createVerticalStrut(20));
-        box.add(stateBox);
-        box.add(Box.createVerticalStrut(20));
-        box.add(buttonBox);
-
-        Box spaceBox = Box.createHorizontalBox();
-        spaceBox.add(Box.createHorizontalStrut(70));
-        spaceBox.add(box);
-        spaceBox.add(Box.createHorizontalStrut(70));
-
-        this.add(spaceBox);
-
-
-    }
-
-}
+//package ui;
+//
+//import model.Task;
+//import model.ToDoList;
+//
+//import javax.swing.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//public class AddTaskDialog extends JDialog {
+//    final int width = 700;
+//    final int height = 700;
+//    private ToDoList toDoList;
+//
+//    public AddTaskDialog(JFrame jf, String title, boolean isModel) {
+//        super(jf, title, isModel);
+//        toDoList = new ToDoList();
+//
+//        //组装视图
+//        this.setLocation(200, 200);
+//
+//        Box box = Box.createVerticalBox();
+//        //组装task
+//        Box taskBox = Box.createHorizontalBox();
+//        JLabel taskLabel = new JLabel("Task");
+//        JTextField taskField = new JTextField(7);
+//        taskBox.add(taskLabel);
+//        taskBox.add(Box.createHorizontalStrut(20));
+//        taskBox.add(taskField);
+//
+//        //组装State
+//
+//        Box stateBox = Box.createHorizontalBox();
+//        JLabel stateLabel = new JLabel("State");
+//        JTextField stateField = new JTextField(7);
+//        stateBox.add(stateLabel);
+//        stateBox.add(Box.createHorizontalStrut(20));
+//        stateBox.add(stateField);
+//
+//        //组装按钮
+//
+//        Box buttonBox = Box.createHorizontalBox();
+//        JButton addButton = new JButton("Add Task");
+//        addButton.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                //get input
+//                String task = taskField.getText().trim();
+//                String state = stateField.getText().trim();
+//                Task t = new Task(task, state);
+//                toDoList.addTask(t);
+//                dispose();
+//
+//            }
+//        });
+//        buttonBox.add(addButton);
+//
+//        //add together
+//        box.add(Box.createVerticalStrut(20));
+//        box.add(taskBox);
+//        box.add(Box.createVerticalStrut(20));
+//        box.add(stateBox);
+//        box.add(Box.createVerticalStrut(20));
+//        box.add(buttonBox);
+//
+//        Box spaceBox = Box.createHorizontalBox();
+//        spaceBox.add(Box.createHorizontalStrut(70));
+//        spaceBox.add(box);
+//        spaceBox.add(Box.createHorizontalStrut(70));
+//
+//        this.add(spaceBox);
+//
+//
+//    }
+//
+//}
--- src/main/ui/MainGUI.java
+++ src/main/ui/MainGUI.java
@@ -1,54 +1,54 @@
-package ui;
-
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-
-public class MainGUI {
-    JFrame jf = new JFrame("ToStart App");
-    final int width = 1000;
-    final int height = 700;
-
-
-    public static void createAndShowGUI() {
-        //设置窗口属性
-        final int width = 1000;
-        final int height = 700;
-        JFrame jf = new JFrame("ToStart App");
-        jf.setVisible(true);
-        jf.setSize(width,height);
-        jf.setBackground(new Color(203, 220, 217));
-        jf.setResizable(false);
-        jf.setLocation(200,200);
-
-        //设置菜单栏
-        JMenuBar menuBar = new JMenuBar();
-        JMenu menu = new JMenu("Quit");
-        JMenuItem menuItem = new JMenuItem("Quit ToStart App");
-        menuItem.addActionListener(new ActionListener() {
-            @Override
-            public void actionPerformed(ActionEvent e) {
-                System.exit(0);
-            }
-        });
-        menu.add(menuItem);
-        menuBar.add(menu);
-        jf.setJMenuBar(menuBar);
-
-        jf.add(new Render(jf));
-
-
-
-    }
-
-    public static void main(String[] args) {
-        //Schedule a job for the event-dispatching thread:
-        //creating and showing this application's GUI.
-        javax.swing.SwingUtilities.invokeLater(new Runnable() {
-            public void run() {
-                createAndShowGUI();
-            }
-        });
-    }
-}
+//package ui;
+//
+//import javax.swing.*;
+//import java.awt.*;
+//import java.awt.event.ActionEvent;
+//import java.awt.event.ActionListener;
+//
+//public class MainGUI {
+//    JFrame jf = new JFrame("ToStart App");
+//    final int width = 1000;
+//    final int height = 700;
+//
+//
+//    public static void createAndShowGUI() {
+//        //设置窗口属性
+//        final int width = 1000;
+//        final int height = 700;
+//        JFrame jf = new JFrame("ToStart App");
+//        jf.setVisible(true);
+//        jf.setSize(width,height);
+//        jf.setBackground(new Color(203, 220, 217));
+//        jf.setResizable(false);
+//        jf.setLocation(200,200);
+//
+//        //设置菜单栏
+//        JMenuBar menuBar = new JMenuBar();
+//        JMenu menu = new JMenu("Quit");
+//        JMenuItem menuItem = new JMenuItem("Quit ToStart App");
+//        menuItem.addActionListener(new ActionListener() {
+//            @Override
+//            public void actionPerformed(ActionEvent e) {
+//                System.exit(0);
+//            }
+//        });
+//        menu.add(menuItem);
+//        menuBar.add(menu);
+//        jf.setJMenuBar(menuBar);
+//
+//        jf.add(new Render(jf));
+//
+//
+//
+//    }
+//
+//    public static void main(String[] args) {
+//        //Schedule a job for the event-dispatching thread:
+//        //creating and showing this application's GUI.
+//        javax.swing.SwingUtilities.invokeLater(new Runnable() {
+//            public void run() {
+//                createAndShowGUI();
+//            }
+//        });
+//    }
+//}
